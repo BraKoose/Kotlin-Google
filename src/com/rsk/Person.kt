33ebb3117e2f00e8634473cd5e33baab1ca1beff
Koose <KooseTheBlogger@gmail.com>
2021-08-20 17:16:52 +0000

    DisplayWithLambda

--- src/com/rsk/Person.kt
+++ src/com/rsk/Person.kt
@@ -1,5 +1,10 @@
 package com.rsk
 
 class Person(var name:String, var age: Int) {
-
+    fun display(){
+        println("Display: $name")
+    }
+    fun displayWithLambda(func: (s:String) -> Unit){
+        func(name)
+    }
 }
--- src/main.kt
+++ src/main.kt
@@ -7,5 +7,11 @@ fun main(args: Array<String>){
 
         println("Name is ${Koose.name} ")
         println("and your age is ${Koose.age}")
-
+    Koose.name= "Kwesi"
+    println("This is your best pal ${Koose.name}")
+    Koose.display()
+    Koose.displayWithLambda(:: printname)
+}
+fun printname(name: String){
+    println(name)
 }
