56fc4c0f99418d7526bd9efee17bd4475ac64046
Koose <KooseTheBlogger@gmail.com>
2021-08-20 15:18:33 +0000

    introduction to kotlin

---
+++ src/com/rsk/Person.kt
@@ -0,0 +1,7 @@
+package com.rsk
+
+class Person {
+    var name: String = " "
+    var age : Int = 20
+
+}
---
+++ src/main.kt
@@ -0,0 +1,11 @@
+import com.rsk.Person
+
+fun main(args: Array<String>){
+    print("Hello Koose")
+
+    var Koose = Person()
+    Koose.name = "Bra Koose"
+    Koose.age = 20
+    println("Name is ${Koose.name} and your age is ${Koose.age}")
+
+}
