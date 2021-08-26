8bbab3f01f5c5b45dd6dbacc1294c90835cf7f9c
Koose <KooseTheBlogger@gmail.com>
2021-08-26 16:36:28 +0000

    class and sub classes with open

--- src/com/rsk/Person.kt
+++ src/com/rsk/Person.kt
@@ -6,9 +6,14 @@ interface Signatory{
     fun sign()
 }
 
-class Person(val name:String, var age:Int) : Signatory{
+open class Person(val name:String, var age:Int) : Signatory{
     override fun sign() = println("$name aged $age can sign the documents")
 }
+
+class Animal (val name: String, var age: Int):Signatory{
+    override fun sign() = println("$name aged $age cannot sign")
+}
+class  Student(name: String, age: Int):Person(name, age)
 fun main(args: Array<String>){
     val p = Person("Koose", 36)
     val a = Animal("Elephant", 20)
@@ -18,6 +23,3 @@ fun main(args: Array<String>){
 }
 
 
-class Animal (val name: String, var age: Int):Signatory{
-    override fun sign() = println("$name aged $age cannot sign")
-}
