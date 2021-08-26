c8830b7aaf5db183c8527266a6052ebb1244fb48
Koose <KooseTheBlogger@gmail.com>
2021-08-26 17:40:31 +0000

    Data classes and properties

--- src/com/rsk/Person.kt
+++ src/com/rsk/Person.kt
@@ -7,13 +7,9 @@ interface Signatory{
     fun sign()
 }
 
-open class Person(val name:String, var age:Int) : Signatory{
- var isMarried = true
+open class Person(val name:String, var age:Int, var  isMarried:Boolean =false) : Signatory{
 
-    constructor(name: String, age: Int, isMarried:Boolean):this(name, age){
-        this.isMarried=isMarried
-    }
-
+ var partnerName:String = ""
     override fun sign() = println("$name aged $age can sign the documents(and their martial status is $isMarried)")
 }
 
@@ -22,13 +18,24 @@ open class Animal (val name: String, var age: Int):Signatory{
 }
 class tree (name: String, age: Int):Animal(name, age)
 
+data class User(val name: String, val email: String, var pNumber:Int)
+
 class  Student(name: String, age: Int):Person(name, age)
 fun main(args: Array<String>){
     val p = Person("Koose", 36)
     val a = Animal("Elephant", 20)
-
+    p.partnerName = "Adwoa"
     a.sign()
     p.sign()
+
+    println("Partner name is ${p.partnerName}")
+    var koose = User("Godfred", "KooseTheBlogger@gmail.com",0)
+    println(koose)
+    val (name, email, pNumber) = koose
+    println("User is $name and email is $email with the Phone Number of $pNumber")
+
+    val otherUser = koose.copy(email="404Solutions@gmail.com")
+    println(otherUser)
 }
 
 
