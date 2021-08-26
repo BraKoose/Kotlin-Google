1b79b197b47d14eee96c5014ea330b8a816b39ec
Koose <KooseTheBlogger@gmail.com>
2021-08-26 16:52:41 +0000

    contructors and secondary contructors

--- src/com/rsk/Person.kt
+++ src/com/rsk/Person.kt
@@ -1,5 +1,6 @@
 package com.rsk
 
+import java.lang.Exception
 import java.lang.UnsupportedOperationException
 
 interface Signatory{
@@ -7,12 +8,20 @@ interface Signatory{
 }
 
 open class Person(val name:String, var age:Int) : Signatory{
-    override fun sign() = println("$name aged $age can sign the documents")
+ var isMarried = true
+
+    constructor(name: String, age: Int, isMarried:Boolean):this(name, age){
+        this.isMarried=isMarried
+    }
+
+    override fun sign() = println("$name aged $age can sign the documents(and their martial status is $isMarried)")
 }
 
-class Animal (val name: String, var age: Int):Signatory{
+open class Animal (val name: String, var age: Int):Signatory{
     override fun sign() = println("$name aged $age cannot sign")
 }
+class tree (name: String, age: Int):Animal(name, age)
+
 class  Student(name: String, age: Int):Person(name, age)
 fun main(args: Array<String>){
     val p = Person("Koose", 36)
