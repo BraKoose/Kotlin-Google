92c804bf681b7fdae4b700c44b7b77718bb86e82
Koose <KooseTheBlogger@gmail.com>
2021-08-29 22:16:14 +0000

    Extensions undertstood

--- src/com/rsk/Extensions.kt
+++ src/com/rsk/Extensions.kt
@@ -3,10 +3,20 @@ package com.rsk
 fun main(agrs: Array<String>){
     //Extension Functions
     val text = "We  Don't BREAK \t STreAks"
-    println(replaceStreak(text))
+    var p = 42
+    var k = 90
+    println(p.kooseMaths())
+    println(k.kooseMaths())
+    println(text.replaceStreak())
+
 }
 
-fun  replaceStreak(value:String):String{
+fun  String.replaceStreak():String{
     var regex = Regex("\\s+")
-    return regex.replace(value, " ")
+    return regex.replace(this, " ")
+}
+
+fun Int.kooseMaths(): Int{
+    var number = Math.max(10, 20)
+    return number.compareTo(100)
 }
