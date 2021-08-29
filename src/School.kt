a53040d023df5dbcc81ad3b5326c772fb3ede81b
Koose <KooseTheBlogger@gmail.com>
2021-08-29 21:40:32 +0000

    Default parameters

--- src/School.kt
+++ src/School.kt
@@ -1,16 +1,13 @@
+@file:JvmName("log")
 package com.rsk
 
-import kotlin.math.max
-
 fun main(args: Array<String>){
-    display("Koose o ")
-    println(Kmaths(20, 30))
+    log("Koose o ")
 }
 
-
-fun display(message:String):Boolean{
-    println(message)
+@JvmOverloads
+fun log(message:String, loglevel: Int= 30):Boolean{
+    println(message + loglevel)
     return true
 }
 
-fun Kmaths(a: Int, b:Int): Int = if (a > b) a else b
---
+++ src/com/rsk/App.java
@@ -0,0 +1,11 @@
+package com.rsk;
+
+
+public class App {
+    //Defualt parameters from kotlin
+    public static void main(String[] args){
+        log.log("i love kins snacks ", 3);
+        log.log("i love kins snacks ");
+        log.log("i love kins snacks ", 38);
+    }
+}
