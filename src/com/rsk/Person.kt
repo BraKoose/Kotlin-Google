c2d968b2c5efb4ee6e3806729c00d0fd10562c9f
Koose <KooseTheBlogger@gmail.com>
2021-08-30 02:07:08 +0000

    Classes and overriding

--- src/com/rsk/Person.kt
+++ src/com/rsk/Person.kt
@@ -1,58 +1,14 @@
 package com.rsk
 
-import java.lang.Exception
-import java.lang.UnsupportedOperationException
+open class Company{
+    //Classes in Kotlin
+    var firs_Name: String = ""
+    var last_Name: String = ""
 
-interface Signatory{
-    fun sign()
+    open fun getName():String = "$firs_Name $last_Name"
 }
 
-open class Person(val name:String, var age:Int, var  isMarried:Boolean =false) : Signatory{
+class Employees:Company(){
+    override fun getName():String{return ""}
 
- var partnerName:String = ""
-    override fun sign() = println("$name aged $age can sign the documents(and their martial status is $isMarried)")
 }
-
-open class Animal (val name: String, var age: Int):Signatory{
-    override fun sign() = println("$name aged $age cannot sign")
-}
-class tree (name: String, age: Int):Animal(name, age)
-
-data class User(val name: String, val email: String, var pNumber:Int)
-
-class  Student(name: String, age: Int):Person(name, age)
-fun main(args: Array<String>){
-    val p = Person("Koose", 36)
-    val a = Animal("Elephant", 20)
-
-    /*so we g to see the code associate with Gideon class
-    *
-    * */
-    val g = Gideon("Cocaine", 20.0)
-    g.weightLbs = 100.0
-    println(g)
-   println(g.drug)
-
-
-    g.weightLbs = 220.0
-    val kilos = g.weightKilos
-    g.weightKilos = 100.0
-    println(kilos)
-    p.partnerName = "Adwoa"
-    a.sign()
-    p.sign()
-
-    println("Partner name is ${p.partnerName}")
-    var koose = User("Godfred", "KooseTheBlogger@gmail.com",0)
-    println(koose)
-    val (name, email, pNumber) = koose
-    println("User is $name and email is $email with the Phone Number of $pNumber")
-
-    val otherUser = koose.copy(email="404Solutions@gmail.com")
-    println(otherUser)
-}
-
-
-
-
-
--- src/com/rsk/Types.kt
+++ src/com/rsk/Types.kt
@@ -2,9 +2,14 @@ package com.rsk
 
 interface Time{
     // Creating Interfaces
+    //Default implementations.
 
     fun setTime(hours: Int, mins: Int = 0 , secs: Int = 0)
     fun setTime(time: KooseTime) = setTime(time.hours)
+}
+interface  EndOfTheWorld{
+    fun setTime(time: KooseTime){}
+
 }
 
 class KooseTime{
@@ -13,6 +18,10 @@ class KooseTime{
     var seconds: Int = 0
 }
 
-class YetTime: Time{
+class YetTime: Time, EndOfTheWorld{
+
+    override fun setTime(time: KooseTime) {
+        super<EndOfTheWorld>.setTime(time)
+    }
     override fun setTime(hours: Int, mins: Int , secs: Int){}
 }
