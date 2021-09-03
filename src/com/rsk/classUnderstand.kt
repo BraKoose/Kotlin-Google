5120571e68ce5d9244c89e248fd8469eab49720b
Koose <KooseTheBlogger@gmail.com>
2021-09-03 20:44:44 +0000

    if-else-if-else

--- src/com/rsk/classUnderstand.kt
+++ src/com/rsk/classUnderstand.kt
@@ -1,8 +1,13 @@
 package com.rsk
 
-fun main(agrs: Array<String>){
+fun main(agrs: Array<String>) {
     val num1 = 10
+    val num4 = 200
     val num2 = 20
-    val result = if (num1 > num2) "$num1 is greater than $num2" else "$num1 is less than $num2"
+    val result = if (num1 > num2 + num4) {
+        "Kosse is smart"
+    } else {
+        "I believe me "
+    }
     println(result)
 }
