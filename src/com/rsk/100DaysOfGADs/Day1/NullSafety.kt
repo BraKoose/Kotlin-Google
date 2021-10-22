b5157c50f8c686a149af2decd088d05471c447ba
Koose <KooseTheBlogger@gmail.com>
2021-10-22 15:36:42 +0000

    smart Casts

--- src/com/rsk/100DaysOfGADs/Day1/NullSafety.kt
+++ src/com/rsk/100DaysOfGADs/Day1/NullSafety.kt
@@ -2,8 +2,14 @@ package com.rsk.com.rsk.`100DaysOfGADs`.Day1
 
 fun main(args: Array<String>){
 
-    // null types of strings
+    // null types of strings in If statements to show nullabilty
     var str: String? = "Kooose"
+    var len = if(str != null) str.length else -1
+    println("str is : $str")
+    println("str length is : $len")
+
     str = null
-    println(str)
+    println("str is : $str")
+    len = if (str != null) str.length else -1
+    println("b length is : $len")
 }
---
+++ src/com/rsk/100DaysOfGADs/Day1/smartCast.kt
@@ -0,0 +1,8 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day1
+
+fun main( args: Array<String>){
+    var string: String? = "404 Solutions"
+    if(string != null) { //smart cast
+        print(string.length)//It works Now
+    }
+}
