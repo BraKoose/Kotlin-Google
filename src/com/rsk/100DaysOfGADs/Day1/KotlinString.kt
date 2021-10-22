d4315b75b0c803c4a2a46316946e55caf0a33bbb
Koose <KooseTheBlogger@gmail.com>
2021-10-22 05:54:29 +0000

    accessing string elements

--- src/com/rsk/100DaysOfGADs/Day1/KotlinString.kt
+++ src/com/rsk/100DaysOfGADs/Day1/KotlinString.kt
@@ -1,2 +1,10 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day1
 
+//Example of accessing String elements
+
+fun main(args: Array<String>){
+    val str = "Hello GADs 2021!"
+    println(str[0])
+    println(str[1])
+    println(str[str.length-1])
+}
