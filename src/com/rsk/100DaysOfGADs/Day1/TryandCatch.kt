6f1cd92f7af9dd0662987b0dec232039cf8b8c09
Koose <KooseTheBlogger@gmail.com>
2021-10-22 14:29:10 +0000

    return the catch of the block

--- src/com/rsk/100DaysOfGADs/Day1/TryandCatch.kt
+++ src/com/rsk/100DaysOfGADs/Day1/TryandCatch.kt
@@ -1,18 +1,16 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day1
 
-import com.rsk.getNumber
+
+// Generation an exception and return the statements of catch block
 
 fun main (args: Array<String>) {
-
-    // String value to int which does not generate any exception and returns
-
-    val str = getNumber("10")
+    val str = getNumber("10.5")
     println(str)
+    }
     fun getNumber(str:String): Int{
-        return try {
+        return try{
             Integer.parseInt(str)
-        }catch (e: ArithmeticException){
+        }catch (e: NumberFormatException){
             0
         }
     }
-}
