9c00fe1b269bcb1e8478387f43d665ad64f61350
Koose <KooseTheBlogger@gmail.com>
2021-10-22 04:08:33 +0000

    100Days of Code

---
+++ src/com/rsk/100DaysOfGADs/KotlinArray.kt
@@ -0,0 +1,15 @@
+package com.rsk.com.rsk.`100DaysOfGADs`
+
+
+// arrayOf Function
+
+var myArray1 = arrayOf(1, 2, 6, 9, 15)
+var myArray2 = arrayOf<Int>(1, 3, 5, 7, 9, 11)
+val myArray3 = arrayOf<String>("Tejas", "Koose", "Andy", "Ansh", "sumit")
+val myArray4 = arrayOf(1, 10, 5, "koose", "adwoa", "bilson")
+
+// intArrayOf FUnction
+
+var myArray5: IntArray = intArrayOf(5, 10, 15, 20, 25)
+
+/
