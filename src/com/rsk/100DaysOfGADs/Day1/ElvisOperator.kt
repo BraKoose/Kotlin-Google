0b86b63236e510580abd3a134f1581b4064b59c8
Koose <KooseTheBlogger@gmail.com>
2021-10-22 21:12:16 +0000

    Elvis operator

--- src/com/rsk/100DaysOfGADs/Day1/ElvisOperator.kt
+++ src/com/rsk/100DaysOfGADs/Day1/ElvisOperator.kt
@@ -1,11 +1,29 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day1
 
+import java.lang.IllegalArgumentException
+
 fun main(args: Array<String>){
-    var str: String? =null
-    var str2: String? = "404 Solutions"
-    var len1: Int = str ?.length ?: -1
-    var len2: Int= str2 ?.length ?: -1
-    println("length of str is ${len1}")
-    println("length of str2 is ${len2}")
+    val StationName:String = station()
+    println(StationName)
 }
-//Elvis Operators intro
+fun station(): String{
+    val str: String? = "Niit"
+    val strLength: Int = if (str != null) str.length else -1
+    val strLength2: Int = str?.length ?: -1
+    var string = "str = $str\n" +
+            "strLength = $strLength\n" +
+            "strLength2 = $strLength2\n\n"
+
+    fun check(textOne: String?, textTwo:String?): String?{
+        val textOne = textOne?: return null
+        val textTwo = textTwo?: IllegalArgumentException("text exception")
+
+        return "\ntextOne = $textOne\n" +
+                "textTwo = $textTwo\n"
+    }
+    string += "check(null, \"Sofoline Western Pass\") = ${check(null, "Sofoline Western Pass")}\n" +
+            "check(\"Sofoline Twedie\", \"Sofoline Western Pass\") = ${check("Sofoline Twedie", "Sofoline Western Pass")}\n"
+    return  string
+
+
+}
