baf2a53a80ddb5f3d0be1f1bb207ae483c261d90
Koose <KooseTheBlogger@gmail.com>
2021-10-22 04:17:46 +0000

    set() underst00d well

--- src/com/rsk/100DaysOfGADs/KotlinArray.kt
+++ src/com/rsk/100DaysOfGADs/KotlinArray.kt
@@ -12,4 +12,30 @@ val myArray4 = arrayOf(1, 10, 5, "koose", "adwoa", "bilson")
 
 var myArray5: IntArray = intArrayOf(5, 10, 15, 20, 25)
 
-/
+/*
+
+Modify and access elments of array with Set() and get() respectively
+
+ */
+
+
+//set() example
+
+fun main (args: Array<String>){
+    val array1 = arrayOf(1, 3, 5, 7, 9)
+    val array2 = arrayOf<Long>(11, 12, 13, 14, 15)
+    array1.set(0, 5)
+    array1[2] = 100
+
+    array2.set(2, 10)
+    array2[3] = 16
+
+    for(element in array1){
+        println(element)
+    }
+    println()
+
+    for (element in array2){
+        println(element)
+    }
+}
