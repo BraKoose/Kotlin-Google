3e58875e3ed542af43252aaa835b726627aa4a44
Koose <KooseTheBlogger@gmail.com>
2021-10-22 18:15:37 +0000

    safe Operator

--- src/com/rsk/100DaysOfGADs/Day1/Unsafe&SafeCast.kt
+++ src/com/rsk/100DaysOfGADs/Day1/Unsafe&SafeCast.kt
@@ -1,8 +1,12 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day1
 
 fun main (args: Array<String>){
-    val obj : String? = "Kotlin is the offical language"
-    val str : String? = obj as String?
-    println(str)
-//STring Unsafe Cast
+    /** It cast the value if possible or return null instead
+    of throwing ecxception even casting is not possible**/
+
+    val location : Any = "kotlin"
+    val safeString: String? = location as?  String
+    val safeInt: Int? = location as? Int
+    println(safeString)
+    println(safeInt)
 }
