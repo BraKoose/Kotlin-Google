05d290b644b3d821f9bfcf2a80f546f8c43e9c6e
Koose <KooseTheBlogger@gmail.com>
2021-10-22 18:01:24 +0000

    smart cast

--- src/com/rsk/100DaysOfGADs/Day1/smartCast.kt
+++ src/com/rsk/100DaysOfGADs/Day1/smartCast.kt
@@ -1,10 +1,12 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day1
 
-fun main( args: Array<String>){
+fun main( args: Array<String>) {
     // The use of "is" in cast
     val obj: Any = "404 Solutions is the startup that made the legacy Trotro App"
-    if (obj is String){
-    //No Explicit casting Needed
+    if (obj !is String) {
+        println("obj is not a string")
+
+    } else
+    //No explicit casting needed
         println("String length is ${obj.length}")
-    }
 }
