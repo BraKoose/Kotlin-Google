20e77bfb3c85e36fe8acaa94270d94c89b15a99e
Koose <KooseTheBlogger@gmail.com>
2021-10-22 18:09:16 +0000

    Unsafe Cast

---
+++ src/com/rsk/100DaysOfGADs/Day1/Unsafe&SafeCast.kt
@@ -0,0 +1,8 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day1
+
+fun main (args: Array<String>){
+    val obj : String? = "Kotlin is the offical language"
+    val str : String? = obj as String?
+    println(str)
+//STring Unsafe Cast
+}
