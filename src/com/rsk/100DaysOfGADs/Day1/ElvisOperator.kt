1e15488a60608b5824ec87e9bc909ae677006866
Koose <KooseTheBlogger@gmail.com>
2021-10-22 18:24:03 +0000

    evlis operators

---
+++ src/com/rsk/100DaysOfGADs/Day1/ElvisOperator.kt
@@ -0,0 +1,11 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day1
+
+fun main(args: Array<String>){
+    var str: String? =null
+    var str2: String? = "404 Solutions"
+    var len1: Int = str ?.length ?: -1
+    var len2: Int= str2 ?.length ?: -1
+    println("length of str is ${len1}")
+    println("length of str2 is ${len2}")
+}
+//Elvis Operators intro
