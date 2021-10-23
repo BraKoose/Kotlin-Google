1d36106e0980fd985c9165e69184f8cf3f2ed480
Koose <KooseTheBlogger@gmail.com>
2021-10-23 23:45:18 +0000

    filled elemeents of ArrayLiSt using Collection

--- src/com/rsk/100DaysOfGADs/Day2/Arraylist.kt
+++ src/com/rsk/100DaysOfGADs/Day2/Arraylist.kt
@@ -1,28 +1,20 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day2
 
 fun main (args: Array<String>){
- // initialize Arraylist capacity
-    val arrayList = ArrayList<String>(10)
-    arrayList.add("Iphone")
-    arrayList.add("Samsung")
-    arrayList.add("Lampstand")
-    arrayList.add("Java")
-    arrayList.add("Kotlin")
-    println(".....println ArrayList.......")
-    for( i in arrayList){
-        println(i)
-    }
-    println("Size of arrayList 1 = " + arrayList.size)
-    val  arrayList1 = ArrayList<Int>(5)
-    arrayList1.add(10)
-    arrayList1.add(90)
-    arrayList1.add(101)
-    arrayList1.add(15)
-    arrayList1.add(2)
-    println(".........println ArrayList1 ...")
-    for( i  in arrayList1){
-        println(i)
-    }
-    println("size of the arraylist1 = " + arrayList1.size)
+    //Fiiled elements in ArrayList Using Collections
+    val arrayList :ArrayList<String> = ArrayList<String>(5)
+    val list: MutableList<String> = mutableListOf<String>()
 
+    list.add("sir Charles")
+    list.add("Skin")
+    list.add("Suleman")
+    list.add("Jay")
+
+    arrayList.addAll(list)
+    println("........println Arraylist .........")
+    val itr = arrayList.iterator()
+    while (itr.hasNext()){
+        println(itr.next())
+    }
+    println("Size of the arrayList = " + arrayList.size)
 }
