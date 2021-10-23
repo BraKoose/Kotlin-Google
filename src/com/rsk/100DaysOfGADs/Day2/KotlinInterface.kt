828a1ec632e4b2de948eb6692319b1441af9fdfa
Koose <KooseTheBlogger@gmail.com>
2021-10-23 14:28:28 +0000

    example 3 kotlin interface

--- src/com/rsk/100DaysOfGADs/Day2/KotlinInterface.kt
+++ src/com/rsk/100DaysOfGADs/Day2/KotlinInterface.kt
@@ -1,15 +1,25 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day2
 
-// listOf(), we can pass different types of data at the same time.
-//List  can also traverse the list using index range
+/*For more specific, we provide the generic types of list
+* such as listOf<int>(), ListOf<String>(),listOf<Any>
+* */
 
 fun main (args: Array<String>){
-    var list = listOf(1,2,3, "Koose", "Scriptures", "LGBT")
-    for (elements in list){
-        println(elements)
+    var intList: List<Int> = listOf<Int>(1, 2, 7)
+    var StringList: List<String> = listOf<String>("404 Solutions", "Clade", "Trotro", "Highway")
+    var anyList: List<Any> = listOf<Any>(1, "Dada Koose", 209, "404 Solutions", 9090, "Love Kotlin")
+    println("Print int List ")
+    for(element in intList){
+        println(element)
     }
     println()
-    for(index in 0 ..list.size-1){
-        println(list[index])
+    println("Print String List")
+    for (words in StringList){
+        println(words)
+    }
+    println()
+    println("Print Any List ")
+    for (anybody in anyList){
+        println(anybody)
     }
 }
