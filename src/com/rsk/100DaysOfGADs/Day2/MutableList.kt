d362783d3928a48e83b0723e9a54e3302358fbe2
Koose <KooseTheBlogger@gmail.com>
2021-10-23 15:52:46 +0000

    Mutable function tried, tested and understood

--- src/com/rsk/100DaysOfGADs/Day2/MutableList.kt
+++ src/com/rsk/100DaysOfGADs/Day2/MutableList.kt
@@ -2,42 +2,65 @@ package com.rsk.com.rsk.`100DaysOfGADs`.Day2
 
 fun main (args: Array<String>){
 /*
-Mutable list can be declared as empty and later add up. This time around all kinds
-of Data Types
- */
-    var listNumber: MutableList<Int> = mutableListOf<Int>()
-    var MlistString: MutableList<String> = mutableListOf<String>()
-    var AnyGangster: MutableList<Any> = mutableListOf<Any>()
+Different Functions of Mutablelist Interface
 
-    listNumber.add(100)
-    listNumber.add(89)
-    listNumber.add(67)
-    listNumber.add(2, 10)
+*/
+  var mutableList = mutableListOf<String>()
+  mutableList.add("Jersey")
+  mutableList.add("Anita")
+  mutableList.add("Kingsford")
 
-    MlistString.add("Tonto",)
-    MlistString.add("Amoateng")
-    MlistString.add("Bullet")
-    MlistString.add("Koo")
-    MlistString.add("Doctor, Doctor")
+  var mutableList2 = mutableListOf<String>("Love", "Hate")
+  var mutableList3 = mutableListOf<String>("R2bess", "Wahali")
+  var mutableList4 = mutableListOf<String>("Omar Sterling", "Life")
 
-    AnyGangster.add("Kooko")
-    AnyGangster.add(2.19)
-    AnyGangster.add("Weed")
-
-    println()
-    println("......... print String type ......")
-    for (words in MlistString){
-        println(words)
+  println(".........mutableList.........")
+  for(elements in mutableList){
+      println(elements)
+  }
+    println("......mutableList[2]....... ")
+    println(mutableList[2])
+    mutableList.add(2, "Soldier")
+    println(".............mutable.add(2, /soldeir.....")
+    for(elements in mutableList){
+        println(elements)
     }
-    println()
-    println(".........print Number Type ..........")
-    for(numbers in listNumber){
-        println(numbers)
+    mutableList.add("No Envy")
+    println("............mutable.add....no nevy ")
+    for(element in mutableList){
+        println(element)
     }
-    println()
-    println(".....print any gangster type.........")
-    for( shattaWale in AnyGangster){
-        println(shattaWale)
+    mutableList.addAll(1, mutableList3)
+    println("..........mutable.add[mutable3]")
+    for(elem in mutableList){
+        println(elem)
     }
+    mutableList.remove("No Envy")
+    println(".....mutablelist.remove No Envy........")
+    for(elements in mutableList){
+        println(elements)
+    }
+    mutableList.removeAll(mutableList2)
+    println(".........mutablelist.remove mutablelist 2......")
+    for(delete in mutableList){
+        println(delete)
+    }
+    println("..........mutablelist.set2....Asko")
+    mutableList.set(2, "ASko")
+    for(asko in mutableList){
+        println(asko)
+    }
+
+    println(".....mutablelist.retainall mutable4 .....")
+    mutableList.retainAll(mutableList4)
+    for(restain in mutableList){
+        println(restain)
+    }
+    println("......mutablelist2 after mutable2.clear ....")
+    println(mutableList2)
+
+    println("..........mutableSublist......")
+
+
 
 }
