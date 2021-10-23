a724e55741816e69a9143dc9c1faf4ba685a3776
Koose <KooseTheBlogger@gmail.com>
2021-10-23 14:20:11 +0000

    Example 2 interface Kotlin

--- src/com/rsk/100DaysOfGADs/Day2/KotlinInterface.kt
+++ src/com/rsk/100DaysOfGADs/Day2/KotlinInterface.kt
@@ -1,10 +1,15 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day2
 
-// listOf() function example
+// listOf(), we can pass different types of data at the same time.
+//List  can also traverse the list using index range
 
 fun main (args: Array<String>){
-    var list = listOf("Koose", "Koko", "Ganduff") //read-only, fixed-size
-    for(elements in list){
+    var list = listOf(1,2,3, "Koose", "Scriptures", "LGBT")
+    for (elements in list){
         println(elements)
+    }
+    println()
+    for(index in 0 ..list.size-1){
+        println(list[index])
     }
 }
