8ed328c2610ae316779073380ea9f167dabe7b79
Koose <KooseTheBlogger@gmail.com>
2021-10-23 23:28:53 +0000

    Arraylist

---
+++ src/com/rsk/100DaysOfGADs/Day2/Arraylist.kt
@@ -0,0 +1,13 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day2
+
+fun main (args: Array<String>){
+    val arrayList = ArrayList<String>()
+    arrayList.add("Kose")
+    arrayList.add("Philip")
+    arrayList.add("Nikohlas")
+    arrayList.add("Mabel")
+    println(".......print arrayList.......")
+    for (i in arrayList){
+        println(i)
+    }
+}
