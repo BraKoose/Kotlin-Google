0631446b85c16e781458ddbbd414d43f70f5a83a
Koose <KooseTheBlogger@gmail.com>
2021-10-23 14:56:51 +0000

    multable list undertstanding so cool

--- src/com/rsk/100DaysOfGADs/Day2/MutableList.kt
+++ src/com/rsk/100DaysOfGADs/Day2/MutableList.kt
@@ -1,13 +1,23 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day2
 
 fun main (args: Array<String>){
-    var mutableList = mutableListOf("Koose", "Chess", "Maths")
+/*
+Mutable list can be declared as empty and later add up.
+ */
+    var mutablelist1 = mutableListOf("Koose", "404 Solutions")
+    mutablelist1.add("Tonto")
+    mutablelist1.add("Mr Bonny")
 
-    for(elements in mutableList){
+    var mutableList2 = mutableListOf<String>()
+    mutableList2.add("Kayna City")
+    mutableList2.add("Alonso")
+    mutableList2.add("Derrick Adu")
+
+    for (elements in mutablelist1){
         println(elements)
     }
     println()
-    for (index in 0..mutableList.size-1){
-        println(mutableList[index])
+    for(badboys in mutableList2){
+        println(badboys)
     }
 }
