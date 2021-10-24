f21db091ab2099f7b41ca73df0feaec99e7db21b
Koose <KooseTheBlogger@gmail.com>
2021-10-24 07:55:48 +0000

    Non Generic types

--- src/com/rsk/100DaysOfGADs/Day3/Map.kt
+++ src/com/rsk/100DaysOfGADs/Day3/Map.kt
@@ -1,9 +1,9 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day3
 
 fun main (args: Array<String>){
-// For specific we can provide genric types
-    val myMap: Map<Int, String> = mapOf<Int, String>(1 to "Koose", 2 to "Koko", 3 to "Learn")
-    for (key in myMap.keys){
-    println("elements at the key $key = ${myMap.get(key)}")
+// For specific we can provide NON - genric types
+    val myMap = mapOf(1 to "Godfred", 4 to "Addai", 10 to "Kayna City", "Kejetia" to "Asuofua", "Abrepo" to 9)
+    for(key in myMap.keys){
+    println("Elements at key $key = ${myMap.get(key)}")
     }
 }
