598889364b78bbf3e707f2a3aa96759ad78180ba
Koose <KooseTheBlogger@gmail.com>
2021-10-25 22:38:09 +0000

    Contain and Contain.All

--- src/com/rsk/100DaysOfGADs/Day4/setInterface.kt
+++ src/com/rsk/100DaysOfGADs/Day4/setInterface.kt
@@ -2,14 +2,16 @@ package com.rsk.com.rsk.`100DaysOfGADs`.Day4
 
 //we create a set of int, type non generic and another generic set of Any Type
 fun main(args: Array<String>) {
-    val intSet = setOf(1, 2, 4, 7, 2002, 20)
-    val mySet: Set<Any> = setOf(2, 5, 7, 12, "Koose", "Nickolas")
-    println("............printInt Set.............")
-    for(elements in intSet){
-        println(elements)
-    }
-    println("...........print Any Set")
-    for(key in mySet){
+    val intSet = setOf(1, 2, 4, 7, 7, 2002, 20)
+    val mySet: Set<Any> = setOf(2, 5, 5, 7, 12, "Koose", "Nickolas")
+
+    println(".....print Any Set/......." )
+    for (key in mySet){
         println(key)
     }
+    println(".....my set.contains Nickolas....")
+    println(mySet.contains("Nickolas"))
+    println(".......myset Contains(2021)....")
+    println(mySet.contains(2021))
+    println(mySet.containsAll(intSet))
 }
