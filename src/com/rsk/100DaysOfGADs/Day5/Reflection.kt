ac33777e0ec250a2f9fa7d726ee9cd7cb964e312
Koose <KooseTheBlogger@gmail.com>
2021-10-27 12:06:13 +0000

    Overloaded function reference operator(::)

--- src/com/rsk/100DaysOfGADs/Day5/Reflection.kt
+++ src/com/rsk/100DaysOfGADs/Day5/Reflection.kt
@@ -1,7 +1,11 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day5
 
-fun main (args: Array<String>){
-    val numbers = listOf(-10, 1, -12, 2, 2)
-    fun isPositive(x: Int)=x>0
+fun main (args: Array<String>) {
+    fun isPostive(s: String) = s == "kotlin" || s =="Kotlin" || s == "Google Kotlin"
+    fun isPositive(x: Int) = x > 0
+    val numbers = listOf(10, 99, -10, -101, -1000)
+    val strings = listOf("Kotlin", "Python")
+
     println(numbers.filter(::isPositive))
+    println(strings.filter (::isPostive))
 }
