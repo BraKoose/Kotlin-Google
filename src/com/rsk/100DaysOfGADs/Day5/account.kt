9c3b9e908df18864f4bdb541fe1a71ad6445cf45
Koose <KooseTheBlogger@gmail.com>
2021-10-27 12:42:22 +0000

    class building example

---
+++ src/com/rsk/100DaysOfGADs/Day5/account.kt
@@ -0,0 +1,13 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day5
+
+class account {
+    var acc_no: Int = 0
+    var name: String? = null
+    var amount:Float = 0f
+        fun deposit(){
+            //deposit code
+        }
+        fun checkBalance(){
+            //balance check code
+        }
+}
