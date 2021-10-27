172e84ee89b8b6622b04f01c7791cad0a25286dc
Koose <KooseTheBlogger@gmail.com>
2021-10-27 13:29:05 +0000

    lamp class

---
+++ src/com/rsk/100DaysOfGADs/Day5/Constructors.kt
@@ -0,0 +1,5 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day5
+
+class Constructors {
+
+}
---
+++ src/com/rsk/100DaysOfGADs/Day5/Lamp.kt
@@ -0,0 +1,26 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day5
+
+class lamp {
+    //property (data member
+    private  var isOn: Boolean = false
+
+    //member function
+    fun turnOn() {
+        isOn = true
+    }
+    fun turnOff(){
+        isOn = false
+    }
+    fun displayLightStatus(){
+        if (isOn == true)
+            println(" lamp is on")
+        else
+            println(" lamp is off")
+    }
+}
+fun main(args: Array<String>){
+   val lamp = lamp()
+    lamp.displayLightStatus()
+
+
+}
