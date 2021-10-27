945a8f04a7d06f5535dc74abe04edc80097083bf
Koose <KooseTheBlogger@gmail.com>
2021-10-27 13:43:26 +0000

    secondarry contructor examples

--- src/com/rsk/100DaysOfGADs/Day5/Constructors.kt
+++ src/com/rsk/100DaysOfGADs/Day5/Constructors.kt
@@ -1,19 +1,12 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day5
 
-class Constructors(name : String, id: Int) {
-    val e_name:String
-    var e_id: Int
-
-    init {
-        e_name = name.capitalize()
-        e_id = id
-
-        println("Name = ${e_name}")
-        println("ID  = ${e_id}")
+class Constructors(password: String){
+    constructor(name: String, id: Int, password: String):this(password){
+        println("Name = ${name}")
+        println("ID - ${id}")
+        println("Password = ${password}")
     }
-
 }
-
 fun  main (args: Array<String>){
-    val myclass = Constructors("Koose", 456)
+    val myclass = Constructors("Koose", 456, "pussy")
 }
