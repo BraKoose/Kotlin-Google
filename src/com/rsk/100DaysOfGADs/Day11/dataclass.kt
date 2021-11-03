f3116591fe9bea4a1b05ef07cd3c6aa505d1af18
Koose <KooseTheBlogger@gmail.com>
2021-11-03 23:07:39 +0000

    data class

---
+++ src/com/rsk/100DaysOfGADs/Day11/dataclass.kt
@@ -0,0 +1,8 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day11
+
+data class dataclass (var name: String, var id: Int, var email:String)
+
+fun main(args: Array<String>){
+    val u = dataclass("Koose", 90, "KooseTheBloogger@gmail.com")
+    println(u)
+}
