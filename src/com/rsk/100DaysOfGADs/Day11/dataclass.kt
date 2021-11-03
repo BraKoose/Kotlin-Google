05d003ede4129e5e6ae0ec567c05694179570211
Koose <KooseTheBlogger@gmail.com>
2021-11-03 23:21:41 +0000

    Default parameters

--- src/com/rsk/100DaysOfGADs/Day11/dataclass.kt
+++ src/com/rsk/100DaysOfGADs/Day11/dataclass.kt
@@ -1,15 +1,9 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day11
 
-data class dataclass (var name: String, var id: Int, var email:String)
+data class dataclass (var name: String = "Ama", var id: Int, var email:String)
 
 fun main(args: Array<String>){
     val u = dataclass("Koose", 90, "KooseTheBloogger@gmail.com")
-    val p = Product("Netflix", 50)
-    val p2 = Product("Gym", 1)
-    val p3 = Product("Gym", 1)
-    println(p)
-    println(p ==p2)
-    println(p2.equals(p3))
     println(u)
 }
 
