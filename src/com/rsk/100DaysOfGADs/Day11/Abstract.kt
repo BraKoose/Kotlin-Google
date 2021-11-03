7fb2c8caedd50a53542ecdda73720b8261b09259
Koose <KooseTheBlogger@gmail.com>
2021-11-03 05:36:47 +0000

    Abstract Car()

--- src/com/rsk/100DaysOfGADs/Day11/Abstract.kt
+++ src/com/rsk/100DaysOfGADs/Day11/Abstract.kt
@@ -1,6 +1,14 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day11
 
-abstract class A {
-    var x = 0
-        abstract fun koose()
+abstract class Car{
+    abstract fun run()
+}
+class Kantanka: Car(){
+    override fun run() {
+        println("Kantanka is running safely.")
+    }
+}
+fun main(args: Array<String>){
+    val obj = Kantanka()
+    obj.run()
 }
