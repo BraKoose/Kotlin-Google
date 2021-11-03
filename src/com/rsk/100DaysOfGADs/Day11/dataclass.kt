41bf0e256f63f6d5f5e3c7dcd4e619f603385628
Koose <KooseTheBlogger@gmail.com>
2021-11-03 23:14:17 +0000

    data class compare

--- src/com/rsk/100DaysOfGADs/Day11/dataclass.kt
+++ src/com/rsk/100DaysOfGADs/Day11/dataclass.kt
@@ -4,5 +4,13 @@ data class dataclass (var name: String, var id: Int, var email:String)
 
 fun main(args: Array<String>){
     val u = dataclass("Koose", 90, "KooseTheBloogger@gmail.com")
+    val p = Product("Netflix", 50)
+    val p2 = Product("Gym", 1)
+    val p3 = Product("Gym", 1)
+    println(p)
+    println(p ==p2)
+    println(p2.equals(p3))
     println(u)
 }
+
+data class Product(var item: String, var price:Int)
