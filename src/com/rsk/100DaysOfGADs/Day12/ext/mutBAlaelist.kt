65c28f53e98aba62b8fe9d56de7d5d089e41984c
Koose <KooseTheBlogger@gmail.com>
2021-11-06 22:48:00 +0000

    swap(0, 2)

---
+++ src/com/rsk/100DaysOfGADs/Day12/ext/mutBAlaelist.kt
@@ -0,0 +1,15 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day12.ext
+
+
+fun MutableList<Int>.swap(index1: Int, index2:Int):MutableList<Int> {
+    val tmp = this[index1] // 'this' represents the lists
+    this[index1] = this[index2]
+    this[index2] = tmp
+    return this
+}
+fun main (args: Array<String>){
+    val list = mutableListOf(5, 10, 15)
+    println("before swapping the list: $list")
+    val result = list.swap(0,2)
+    println("after swapping the list: $result")
+}
