683854de4ab3705e7e699201d202d67a32f123c2
Koose <KooseTheBlogger@gmail.com>
2021-11-06 22:52:40 +0000

    nullables extnt functions

--- src/com/rsk/100DaysOfGADs/Day12/ext/mutBAlaelist.kt
+++ src/com/rsk/100DaysOfGADs/Day12/ext/mutBAlaelist.kt
@@ -1,11 +1,14 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day12.ext
 
 
-fun MutableList<Int>.swap(index1: Int, index2:Int):MutableList<Int> {
-    val tmp = this[index1] // 'this' represents the lists
-    this[index1] = this[index2]
-    this[index2] = tmp
-    return this
+fun MutableList<Int>?.swap(index1: Int, index2:Int):Any {
+    if (this == null)return "null"
+    else{
+        val tmp = this[index1]//'this' represents to the list
+        this[index1] = this [index2]
+        this[index2] = tmp
+        return this
+    }
 }
 fun main (args: Array<String>){
     val list = mutableListOf(5, 10, 15)
