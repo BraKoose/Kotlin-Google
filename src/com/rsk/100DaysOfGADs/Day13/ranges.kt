62724433619c8ca3078f7a0bf607350b1198d9fc
Koose <KooseTheBlogger@gmail.com>
2021-11-06 23:36:35 +0000

    iterator

--- src/com/rsk/100DaysOfGADs/Day13/ranges.kt
+++ src/com/rsk/100DaysOfGADs/Day13/ranges.kt
@@ -1,9 +1,10 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day13
 
 fun main (args: Array<String>) {
-for (x in 'a' ..'e')
-    print("$x ")
-    println()
-    for (x in 'e' downTo 'a')
+    val chars = ('a' .. 'e')
+    val it =chars.iterator()
+    while (it.hasNext()){
+        val x = it.next()
         print("$x ")
+    }
 }
