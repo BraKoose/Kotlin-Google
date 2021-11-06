f8b8fe2698b04a2bf0cb647d3037cbd1967edfb7
Koose <KooseTheBlogger@gmail.com>
2021-11-06 23:24:22 +0000

    ranges

---
+++ src/com/rsk/100DaysOfGADs/Day13/ranges.kt
@@ -0,0 +1,16 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day13
+
+fun main (args: Array<String>){
+    for (a in 10 until 39){
+        print(a)
+    }
+    println()
+
+    for (x in 'a' .. 'f'){
+        print(x)
+    }
+    println()
+    val range = 1.0 .. 6.0
+    println(range)
+    println("3.14 in range is ${3.14 in range}")
+}
