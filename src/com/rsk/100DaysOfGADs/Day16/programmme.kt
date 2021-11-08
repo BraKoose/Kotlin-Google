f836f7162b03bd9330ead15e87bf10843677e0c3
Koose <KooseTheBlogger@gmail.com>
2021-11-08 06:19:06 +0000

    Classes Generic types

--- src/com/rsk/100DaysOfGADs/Day16/programmme.kt
+++ src/com/rsk/100DaysOfGADs/Day16/programmme.kt
@@ -4,8 +4,17 @@ fun main(args: Array<String>){
     var names:List<String> = listOf("Joe Smith", "Tejas", "Andy")
     val name = names.itemAt(2)
     println(name)
+
+    val n:Node<Int> = Node(30)
+    println(n.value())
 }
 
 fun <T> List<T>.itemAt(ndx:Int):T{
     return this[ndx]
+}
+
+class Node<T>(private val item:T){
+    fun value():T{
+        return item
+    }
 }
