4debfdef60671bb87a8c3f5807eb4c3c77faac96
Koose <KooseTheBlogger@gmail.com>
2021-11-23 18:39:06 +0000

    for each is better for sum()

--- src/com/rsk/100DaysOfGADs/Day29/Recaps.kt
+++ src/com/rsk/100DaysOfGADs/Day29/Recaps.kt
@@ -2,7 +2,14 @@ package com.rsk.com.rsk.`100DaysOfGADs`.Day29
 
 fun main(){
     val givenArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    //using fold method
-    println(givenArray.fold(0 ){acc, i -> acc + i  })
+
+    var sum = 0
+
+    givenArray.forEach {
+        sum += it
+    }
+    //print the sum
+    println(sum)
 }
+
 
