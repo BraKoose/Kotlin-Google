7c88fd0db23e831939c530441c08cee9c256ff0e
Koose <KooseTheBlogger@gmail.com>
2021-11-23 18:41:38 +0000

    reduce acc on a line mthod

--- src/com/rsk/100DaysOfGADs/Day29/Recaps.kt
+++ src/com/rsk/100DaysOfGADs/Day29/Recaps.kt
@@ -3,13 +3,8 @@ package com.rsk.com.rsk.`100DaysOfGADs`.Day29
 fun main(){
     val givenArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-    var sum = 0
-
-    givenArray.forEach {
-        sum += it
-    }
-    //print the sum
-    println(sum)
+    //it will print the sum
+    println(givenArray.reduce{acc, i -> acc + i  })
 }
 
 
