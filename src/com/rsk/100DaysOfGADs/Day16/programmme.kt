39e461e101b00f23f671af92fbf178b29e96327d
Koose <KooseTheBlogger@gmail.com>
2021-11-23 18:32:25 +0000

    recap arrays

--- src/com/rsk/100DaysOfGADs/Day16/programmme.kt
+++ src/com/rsk/100DaysOfGADs/Day16/programmme.kt
@@ -13,7 +13,7 @@ fun <T> List<T>.itemAt(ndx:Int):T{
     return this[ndx]
 }
 
-class Node<T>(private val item:T){
+class Node<T: Number>(private val item:T){
     fun value():T{
         return item
     }
---
+++ src/com/rsk/100DaysOfGADs/Day29/Recaps.kt
@@ -0,0 +1,6 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day29
+
+fun main(){
+    val givenArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    println(givenArray.sum())
+}
