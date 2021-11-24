18d3c05a318ec65de5bb91d5feb8b42bf1d8f4ab
Koose <KooseTheBlogger@gmail.com>
2021-11-24 12:03:53 +0000

    using when statments for checkNumber

--- src/com/rsk/100DaysOfGADs/Day30/checkNumber.kt
+++ src/com/rsk/100DaysOfGADs/Day30/checkNumber.kt
@@ -4,14 +4,13 @@ import java.util.*
 
 //main fun, entry point of Program
 fun main(args: Array<String>){
-    val num = 10
+    val num = -900
     //using if else
 
-    println("Using if else......")
-    if (num > 0 )
-        println("$num is postive number ")
-    else if (num < 0)
-        println("$num is Negative number ")
-    else
-        println("Entered Number is zero which means neutral ")
+println("Using when Statement....")
+    when{
+        num > 0 -> println("$num is a Postive Number")
+        num < 0 -> println("$num is a negative Number")
+        else -> println("The Enetered number is zero")
+    }
 }
