95b38752d988bf1dae28f94f77837b10ed20120c
Koose <KooseTheBlogger@gmail.com>
2021-11-24 11:51:48 +0000

    revered array without a new variable to do that, with reverese()

--- src/com/rsk/100DaysOfGADs/Day29/Recaps.kt
+++ src/com/rsk/100DaysOfGADs/Day29/Recaps.kt
@@ -3,8 +3,8 @@ package com.rsk.com.rsk.`100DaysOfGADs`.Day29
 import java.util.Arrays
 
 fun main(args: Array<String>){
-    val numberArray: IntArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    var reservedArray = numberArray.reversedArray()
-    println(Arrays.toString(reservedArray))
+    val numberArray: IntArray = intArrayOf(10, 20, 30, 40, 50, 60, 70, 80, 90)
+    numberArray.reverse()
+    println(Arrays.toString(numberArray))
 }
 
