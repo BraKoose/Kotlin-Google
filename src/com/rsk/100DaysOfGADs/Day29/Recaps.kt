9c57a0078688510fb8dc3ebcfc67b11fa9a7c971
Koose <KooseTheBlogger@gmail.com>
2021-11-24 11:49:13 +0000

    reserved arrays with Java utils

--- src/com/rsk/100DaysOfGADs/Day29/Recaps.kt
+++ src/com/rsk/100DaysOfGADs/Day29/Recaps.kt
@@ -1,10 +1,10 @@
 package com.rsk.com.rsk.`100DaysOfGADs`.Day29
 
-fun main(){
-    val givenArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+import java.util.Arrays
 
-    //it will print the sum
-    println(givenArray.reduce{acc, i -> acc + i  })
+fun main(args: Array<String>){
+    val numberArray: IntArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    var reservedArray = numberArray.reversedArray()
+    println(Arrays.toString(reservedArray))
 }
-
 
