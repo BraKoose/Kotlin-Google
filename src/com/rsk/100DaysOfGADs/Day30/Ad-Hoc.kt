798aa72cde0d4a475afed5c997af3d79292c1462
Koose <KooseTheBlogger@gmail.com>
2021-11-24 19:26:10 +0000

    ad-hoc Structs

---
+++ src/com/rsk/100DaysOfGADs/Day30/Ad-Hoc.kt
@@ -0,0 +1,18 @@
+package com.rsk.com.rsk.`100DaysOfGADs`.Day30
+
+
+
+fun main(){
+    val customer = mapOf(
+            "name" to "Bra Koose",
+            "age" to 21,
+            "languages" to listOf("Python", "Kotlin", "Bash"),
+            "address" to mapOf(
+                    "city" to "Accra",
+                    "Street" to "Gye Nyame Street",
+                    "zip code" to "Taifa"
+            )
+
+    )
+    println(customer)
+}
