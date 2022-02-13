3aa668b3acecceb190ad62c3ff0a4bb02468e540
Koose <KooseTheBlogger@gmail.com>
2022-02-13 10:07:34 +0000

    visible modifiers

---
+++ src/com/rsk/ISpace100Days/VisibilityModifiers.kt
@@ -0,0 +1,12 @@
+package com.rsk.com.rsk.ISpace100Days
+
+internal open class Foo {
+    internal fun boo() = println("boo")
+    internal fun doo() = println("doo")
+}
+internal fun Foo.bar() {
+    boo()
+    doo()
+}
+fun main(args: Array<String>) {
+    var fu =
