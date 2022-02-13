1625b18bc41f8a805d170721945154fecadcde86
Koose <KooseTheBlogger@gmail.com>
2022-02-13 10:21:02 +0000

    Obj declarations

--- src/com/rsk/ISpace100Days/VisibilityModifiers.kt
+++ src/com/rsk/ISpace100Days/VisibilityModifiers.kt
@@ -8,5 +8,25 @@ internal fun Foo.bar() {
     boo()
     doo()
 }
+
+//Obj declare
+
+object Util {
+    var name = ""
+        set(value) {
+            field = value
+        }
+    init {
+        println("Initializing Util")
+    }
+    fun foo() = println(name)
+}
+
+
 fun main(args: Array<String>) {
-    var fu =
+    var fu = Foo()
+    fu.bar()
+
+    Util.name = "Bar"
+    Util.foo() // prints "Bar"
+}
