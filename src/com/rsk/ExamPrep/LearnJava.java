17430561f3f19a11fefa9859619c68945f2f0fbc
Koose <KooseTheBlogger@gmail.com>
2022-04-16 09:59:58 +0000

    Learning Java basic syntax

--- src/com/rsk/ExamPrep/LearnJava.java
+++ src/com/rsk/ExamPrep/LearnJava.java
@@ -3,5 +3,15 @@ package com.rsk.ExamPrep;
 public class LearnJava {
     public static void main(String[] args){
         System.out.println("Hello Koose");
+        System.out.print("Koose and ");
+        System.out.print("Koko");
+
+        //type Syntax Java
+        String Koose = "I love coding";
+        final int Age = 22;
+        float trotroFare = 2.99f;
+        System.out.println(Koose);
+        boolean mybool = true;
+        char koose = '$';
     }
 }
