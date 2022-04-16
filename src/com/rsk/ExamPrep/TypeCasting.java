609575ae7b856919066e6ff5cb12add097f8834a
Koose <KooseTheBlogger@gmail.com>
2022-04-16 13:12:59 +0000

    conditions

--- src/com/rsk/ExamPrep/TypeCasting.java
+++ src/com/rsk/ExamPrep/TypeCasting.java
@@ -17,5 +17,26 @@ public class TypeCasting {
 
          String txt = "The zeal is learn and \t prove thy self and God only.";
          System.out.println(txt);
+
+         //Maths casted to int
+         int pop = (int) Math.sqrt(64);
+         System.out.println(pop);
+
+
+         int x = 5;
+
+         String name;
+         name = "Koose";
+         int time = 22;
+
+         if(time < 10) {
+             System.out.println("Good Morning");
+         }
+         else if (time < 16){
+             System.out.println("Good Day");
+             }
+         else {
+             System.out.println("Good Evening" + name);
+         }
     }
 }
