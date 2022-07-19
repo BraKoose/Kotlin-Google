3514dde958855fa3352b570bc60021ea58fdab98
Koose <KooseTheBlogger@gmail.com>
2022-07-19 20:21:05 +0000

    start today

---
+++ src/com/rsk/ExamPrep/JavaScript.java
@@ -0,0 +1,8 @@
+package com.rsk.ExamPrep;
+
+public class JavaScript {
+    public static void main(String[] args){
+        System.out.println("Safana");
+    }
+
+}
--- src/com/rsk/Extensions.kt
+++ src/com/rsk/Extensions.kt
@@ -3,8 +3,8 @@ package com.rsk
 fun main(agrs: Array<String>){
     //Extension Functions
     val text = "We  Don't BREAK \t STreAks"
-    var p = 42
-    var k = 90
+    val p = 42
+    val k = 90
     println(p.kooseMaths())
     println(k.kooseMaths())
     println(text.replaceStreak())
@@ -12,11 +12,11 @@ fun main(agrs: Array<String>){
 }
 
 fun  String.replaceStreak():String{
-    var regex = Regex("\\s+")
+    val regex = Regex("\\s+")
     return regex.replace(this, " ")
 }
 
 fun Int.kooseMaths(): Int{
-    var number = Math.max(10, 20)
+    val number = Math.max(10, 20)
     return number.compareTo(100)
 }
